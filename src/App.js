c1ab7beb2ce5ad13d466a584df690a84ed5533c8
luffy119 <bmgibson22@gmail.com>
2021-12-07 23:02:35 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './Home';
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />}>  
+            </Route>
+          </Routes>
+        </div>
+      </div>
+    </Router>
+   
+  );
+}
+
+export default App;
